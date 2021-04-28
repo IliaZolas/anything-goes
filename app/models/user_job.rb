e53f7a869d8498d18c46c4072da63f2ff1a5914e
Maxime <maxime.personnic@hotmail.fr>
2021-04-28 15:29:58 +0200

    FIX [UserJob User] associations

--- app/models/user_job.rb
+++ app/models/user_job.rb
@@ -1,7 +1,6 @@
 class UserJob < ApplicationRecord
-    # belongs_to :user
-    # belongs_to :job
-    has_many :jobs
-    has_many :users
+    belongs_to :user
+    belongs_to :job
+
     has_one_attached :photo
 end
